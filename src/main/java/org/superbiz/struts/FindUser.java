217d16f1b433afb958d2c8de4a639ebf20d08c77
Victor Ferreira de Matos <victor.matos@furukawaelectric.com>
2019-06-24 15:47:24 -0600

    Replatforming a Struts Application
    Bootfying a Struts app to run on PCF with some Spring facilities.

--- src/main/java/org/superbiz/struts/FindUser.java
+++ src/main/java/org/superbiz/struts/FindUser.java
@@ -17,15 +17,20 @@
  */
 package org.superbiz.struts;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import java.util.Properties;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FindUser {
 
     private int id;
     private String errorMessage;
     private User user;
+
+    private final UserService userService;
+
+    public FindUser(UserService userService) {
+        this.userService = userService;
+    }
 
     public User getUser() {
         return user;
@@ -43,7 +48,7 @@ public class FindUser {
         this.errorMessage = errorMessage;
     }
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
@@ -54,13 +59,8 @@ public class FindUser {
     public String execute() {
 
         try {
-            UserService service = null;
-            Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY,
-                "org.apache.openejb.core.LocalInitialContextFactory");
-            Context ctx = new InitialContext(props);
-            service = (UserService) ctx.lookup("UserServiceImplLocal");
-            this.user = service.find(id);
+
+            this.user = userService.find(id);
         } catch (Exception e) {
             this.errorMessage = e.getMessage();
             return "failure";
---
+++ src/main/java/org/superbiz/struts/Index.java
@@ -0,0 +1,8 @@
+package org.superbiz.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Index extends ActionSupport {
+
+
+}
